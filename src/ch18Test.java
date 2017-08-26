aeffb63b2c0140574d853b4b54f2b13a48dc34c8
bp2505 <jpawbk@gmail.com>
2017-08-26 03:35:04 -0400

    Initial commit

---
+++ src/ch18Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+
+
+class ch18Test {
+    @Test
+    void reverseDisplay() throws Exception {
+        String input = "abcd";
+        ch18.reverseDisplay(input);
+    }
+
+}
